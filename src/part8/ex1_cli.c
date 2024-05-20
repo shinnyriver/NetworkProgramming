e99cd05263b09b7d39b578e2c591deb4c1678105
Changsu Park <gd12091@gmail.com>
2024-05-20 14:03:21 +0900

    fixed untracted files

--- src/part8/ex1_cli.c
+++ src/part8/ex1_cli.c
@@ -26,35 +26,28 @@ int main(int argc, char *argv[]) {
     pid_t pid = getpid();
     request_t request;
 
-    // 클라이언트 FIFO 이름 설정
     snprintf(client_fifo, BUFFER_SIZE, "fifo.%d", pid);
 
-    // 클라이언트 FIFO 생성
     mkfifo(client_fifo, 0666);
 
-    // 요청 설정
     request.pid = pid;
     strncpy(request.filepath, argv[1], BUFFER_SIZE);
 
-    // 서버 FIFO 열기
     serv_fd = open(SERV_FIFO, O_WRONLY);
     if (serv_fd < 0) {
         perror("open serv_fifo");
         exit(1);
     }
 
-    // 요청 전송
     write(serv_fd, &request, sizeof(request));
     close(serv_fd);
 
-    // 클라이언트 FIFO 열기
     client_fd = open(client_fifo, O_RDONLY);
     if (client_fd < 0) {
         perror("open client_fifo");
         exit(1);
     }
 
-    // 서버로부터 파일 내용 읽기
     int n;
     while ((n = read(client_fd, buffer, BUFFER_SIZE)) > 0) {
         write(STDOUT_FILENO, buffer, n);
--- src/part8/ex1_serv.c
+++ src/part8/ex1_serv.c
@@ -20,32 +20,27 @@ int main() {
     char buffer[BUFFER_SIZE];
     request_t request;
 
-    // fifo.serv 생성
     mkfifo(SERV_FIFO, 0666);
 
     while (1) {
-        // fifo.serv 열기
         serv_fd = open(SERV_FIFO, O_RDONLY);
         if (serv_fd < 0) {
             perror("open serv_fifo");
             exit(1);
         }
 
-        // 요청 읽기
         read(serv_fd, &request, sizeof(request));
         close(serv_fd);
 
-        // 클라이언트 FIFO 이름 설정
         snprintf(client_fifo, BUFFER_SIZE, "fifo.%d", request.pid);
 
-        // 클라이언트 FIFO 열기
         client_fd = open(client_fifo, O_WRONLY);
         if (client_fd < 0) {
             perror("open client_fifo");
             continue;
         }
 
-        // 요청된 파일 열기
+
         file_fd = open(request.filepath, O_RDONLY);
         if (file_fd < 0) {
             perror("open file");
@@ -54,7 +49,6 @@ int main() {
             continue;
         }
 
-        // 파일 내용 클라이언트에 전달
         int n;
         while ((n = read(file_fd, buffer, BUFFER_SIZE)) > 0) {
             write(client_fd, buffer, n);
@@ -64,7 +58,6 @@ int main() {
         close(client_fd);
     }
 
-    // fifo.serv 제거
     unlink(SERV_FIFO);
     return 0;
 }
